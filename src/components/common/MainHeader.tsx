4d9d8977bf67e496f72fc97d232dc919de4bdcea
iseeyouha <barditodev@hotmail.com>
2025-04-28 10:22:52 -0500

    feat: config middleware

--- src/components/common/MainHeader.tsx
+++ src/components/common/MainHeader.tsx
@@ -6,28 +6,36 @@ import { faGear } from "@fortawesome/free-solid-svg-icons/faGear"
 import { faCircleUser } from "@fortawesome/free-solid-svg-icons/faCircleUser"
 import Link from "next/link"
 import { HEADER_HEIGHT } from "@/utils/constants/layout"
+import { auth } from "@/lib/auth"
+import { headers } from "next/headers"
 
-const MainHeader = () => (
-  <header
-    style={{ height: HEADER_HEIGHT }}
-    className="home top-0 left-0 z-10 flex items-center p-4 w-full bg-primary-blue text-white"
-  >
-    <div className="flex justify-center items-center w-[50px] h-[50px]">
-      <FontAwesomeIcon icon={faCircleUser} className="text-[50px]" />
-    </div>
-    <div className="ml-3">
-      <p className="text-lg font-bold">Hola, Alberto</p>
-      <p className="text-sm">¡Hola de nuevo!</p>
-    </div>
-    <div className="ml-auto flex gap-4">
-      <Link href="/notifications">
-        <FontAwesomeIcon icon={faBell} className="text-xl" />
-      </Link>
-      <Link href="/configurations">
-        <FontAwesomeIcon icon={faGear} className="text-xl" />
-      </Link>
-    </div>
-  </header>
-)
+const MainHeader = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers()
+  })
+
+  return (
+    <header
+      style={{ height: HEADER_HEIGHT }}
+      className="home top-0 left-0 z-10 flex items-center p-4 w-full bg-primary-blue text-white"
+    >
+      <div className="flex justify-center items-center w-[50px] h-[50px]">
+        <FontAwesomeIcon icon={faCircleUser} className="text-[50px]" />
+      </div>
+      <div className="ml-3">
+        <p className="text-lg font-bold">Hola, {session?.user.name}</p>
+        <p className="text-sm">¡Hola de nuevo!</p>
+      </div>
+      <div className="ml-auto flex gap-4">
+        <Link href="/notifications">
+          <FontAwesomeIcon icon={faBell} className="text-xl" />
+        </Link>
+        <Link href="/configurations">
+          <FontAwesomeIcon icon={faGear} className="text-xl" />
+        </Link>
+      </div>
+    </header>
+  )
+}
 
 export default MainHeader
